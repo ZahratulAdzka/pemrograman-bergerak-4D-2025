5d96f315930b06d9cf1ba0eec881b7635d3ecc21
ZahratulAdzka <adzkanafisa@gmail.com>
2025-04-14 20:56:54 +0700

    230441100165-ZAHRATUL ADZKA NAFISA HAFIZH-ASPRAK KAK RAKHA

---
+++ Tugas Dart/tugas-230441100165-ZAHRATUL ADZKA NAFISA HAFIZH-ASPRAK KAK RAKHA/2304411100165_ZAHRATUL ADZKA N.H._ASPRAK KAK RAKHA/bin/exam3/exam3.dart
@@ -0,0 +1,28 @@
+Set uniqueElement(List<int> myList) {
+  // TODO 1
+    return myList.toSet();
+  // End of TODO 1
+}
+
+Map<String, String> buildFutsalPlayersMap() {
+  // TODO 2
+
+  return {
+    'Goalkeaper': 'Andri',
+    'Anchor': 'Irfan',
+    'Pivot': 'Fikri',
+    'Right Flank': 'Aidi',
+    'Right Flank': 'Hafid',
+  };
+  // End of TODO 2
+}
+
+Map<String, String> updatePivotPlayer() {
+  final futsalPlayers = buildFutsalPlayersMap();
+
+  // TODO 3
+  futsalPlayers['Pivot']= 'Fajar';
+  // End of TODO 3
+
+  return futsalPlayers;
+}
